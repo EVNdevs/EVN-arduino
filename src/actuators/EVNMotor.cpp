b41e12098f39e8433d41f1702bc6f0760751d4ce
HTY2003 <tengyi.maker@gmail.com>
2025-02-16 17:56:59 +0800

    Replace chained PID controllers with single controller for EVNDrivebase

--- src/actuators/EVNMotor.cpp
+++ src/actuators/EVNMotor.cpp
@@ -426,20 +426,25 @@ void EVNMotor::compute_ppr_derived_values_unsafe() volatile
 	_encoder._360000000_div_ppr = 360000000 / _encoder.ppr;
 }
 
-void EVNMotor::runPWM(float duty_cycle_pct) volatile
+void EVNMotor::runPWM_unsafe(float duty_cycle) volatile
 {
-	if (!timerisr_enabled) return;
-	EVNCoreSync0.core0_enter();
-	disable_connected_drivebase_unsafe();
-
-	duty_cycle_pct = constrain(duty_cycle_pct, -100, 100) / 100;
 
 	_pid_control.run_pwm = true;
 	_pid_control.run_speed = false;
 	_pid_control.run_time = false;
 	_pid_control.run_pos = false;
 
-	runPWM_static(&_pid_control, duty_cycle_pct);
+	runPWM_static(&_pid_control, duty_cycle);
+}
+
+void EVNMotor::runPWM(float duty_cycle_pct) volatile
+{
+	if (!timerisr_enabled) return;
+	EVNCoreSync0.core0_enter();
+	disable_connected_drivebase_unsafe();
+
+	float duty_cycle = constrain(duty_cycle_pct, -100, 100) / 100;
+	runPWM_unsafe(duty_cycle);
 
 	EVNCoreSync0.core0_exit();
 }
@@ -632,8 +637,28 @@ EVNDrivebase::EVNDrivebase(float wheel_dia, float axle_track, EVNMotor* motor_le
 	else
 		db.motor_type = CUSTOM_MOTOR;
 
-	db.turn_rate_pid = new PIDController(DRIVEBASE_KP_TURN_RATE, 0, DRIVEBASE_KD_TURN_RATE, DIRECT);
-	db.speed_pid = new PIDController(DRIVEBASE_KP_SPEED, 0, DRIVEBASE_KD_SPEED, DIRECT);
+	switch (db.motor_type)
+	{
+		case EV3_LARGE:
+			db.turn_rate_pid = new PIDController(EV3_LARGE_KP, 0, EV3_LARGE_KD, DIRECT);
+			db.speed_pid = new PIDController(EV3_LARGE_KP, 0, EV3_LARGE_KD, DIRECT);
+			break;
+
+		case EV3_MED:
+			db.turn_rate_pid = new PIDController(EV3_MED_KP, 0, EV3_MED_KD, DIRECT);
+			db.speed_pid = new PIDController(EV3_MED_KP, 0, EV3_MED_KD, DIRECT);
+			break;
+
+		case NXT_LARGE:
+			db.turn_rate_pid = new PIDController(NXT_LARGE_KP, 0, NXT_LARGE_KD, DIRECT);
+			db.speed_pid = new PIDController(NXT_LARGE_KP, 0, NXT_LARGE_KD, DIRECT);
+			break;
+
+		case CUSTOM_MOTOR:
+			db.turn_rate_pid = new PIDController(CUSTOM_KP, 0, CUSTOM_KD, DIRECT);
+			db.speed_pid = new PIDController(CUSTOM_KP, 0, CUSTOM_KD, DIRECT);
+			break;
+	}
 
 	db.speed_accel = fabs(DRIVEBASE_SPEED_ACCEL);
 	db.speed_decel = fabs(DRIVEBASE_SPEED_DECEL);
