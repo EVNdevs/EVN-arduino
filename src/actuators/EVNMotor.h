a27d7d2b6ef584d05fbbbbfaefeab0b80dfce888
HTY2003 <tengyi.maker@gmail.com>
2025-06-28 05:48:22 +0800

    Handle battery voltage readings of 0, and revert some position stop checks

--- src/actuators/EVNMotor.h
+++ src/actuators/EVNMotor.h
@@ -561,22 +561,11 @@ private:
 
 					if (pidArg->target_pos == pidArg->end_pos)
 					{
-						if (!pidArg->hit_end)
-						{
-							pidArg->hit_end = true;
-							pidArg->hit_end_time_us = pidArg->last_update;
-						}
-
-						if (fabs(pidArg->error) <= MOTOR_POS_MIN_ERROR_MOTOR_DEG
-						|| pidArg->last_update - pidArg->hit_end_time_us >= MOTOR_POS_TIMEOUT_US)
+						if (fabs(pidArg->error) <= MOTOR_POS_MIN_ERROR_MOTOR_DEG)
 						{
 							stopAction_static(pidArg, encoderArg, pos, dps);
 							return;
 						}
-					}
-					else
-					{
-						pidArg->hit_end = false;
 					}
 				}
 			}
@@ -1269,25 +1258,15 @@ private:
 				//ideally, we should only stop when both errors are in acceptable range
 				//however, our control scheme might only hit both targets SOME of the time
 				//so we count it as complete when the motor is already targeting the angle and distance endpoints, and either error is acceptable
-				//TODO: Add timeout if both errors cannot be achieved?
 				if (arg->target_angle == arg->end_angle
 				&& arg->target_distance == arg->end_distance)
 				{
-					if (!arg->hit_end)
-					{
-						arg->hit_end = true;
-						arg->hit_end_time_us = arg->last_update;
-					}
-
 					if (fabs(arg->angle_error) <= DRIVEBASE_POS_MIN_ERROR_MOTOR_DEG
-					|| fabs(arg->speed_error) <= DRIVEBASE_POS_MIN_ERROR_MOTOR_DEG
-					|| arg->last_update - arg->hit_end_time_us >= DRIVEBASE_POS_TIMEOUT_US)
+					|| fabs(arg->speed_error) <= DRIVEBASE_POS_MIN_ERROR_MOTOR_DEG)
 					{
 						stopAction_static(arg);
 						return;
 					}
-				} else {
-					arg->hit_end = false;
 				}
 			}
 
